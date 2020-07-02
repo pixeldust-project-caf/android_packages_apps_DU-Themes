shivatejapeddi/Themes

Navbar: Introduce navbar styles [2/3]
Change-Id: I13d320f72db8a57a221ca143c4d6c01eea1db934

src/com/dirtyunicorns/themes/utils/Utils.java
@@ -42,10 +42,13 @@ import android.content.SharedPreferences;
 import android.content.om.IOverlayManager;
 import android.content.pm.PackageManager;
 import android.os.RemoteException;
+import android.provider.Settings;
 import android.text.TextUtils;
 import android.widget.Button;
 
 import androidx.preference.PreferenceManager;
+
+import com.android.internal.util.du.ThemesUtils;
 
 import com.dirtyunicorns.themes.R;
 import com.dirtyunicorns.themes.receivers.ThemesEndReceiver;
@@ -178,12 +181,12 @@ public class Utils {
                 .getBoolean(PREF_THEME_SCHEDULED_REPEAT_DAILY, false)) {
             if (mAlarmMgr != null) {
                 mAlarmMgr.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, getEndTime(context),
-                    mEndPendingIntent);
+                        mEndPendingIntent);
             }
         } else {
             if (mAlarmMgr != null) {
                 mAlarmMgr.setRepeating(AlarmManager.RTC_WAKEUP, getEndTime(context),
-                    AlarmManager.INTERVAL_DAY, mEndPendingIntent);
+                        AlarmManager.INTERVAL_DAY, mEndPendingIntent);
             }
         }
     }
@@ -196,12 +199,12 @@ public class Utils {
                 .getBoolean(PREF_THEME_SCHEDULED_REPEAT_DAILY, false)) {
             if (mAlarmMgr != null) {
                 mAlarmMgr.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, getStartTime(context),
-                    mStartPendingIntent);
+                        mStartPendingIntent);
             }
         } else {
             if (mAlarmMgr != null) {
                 mAlarmMgr.setRepeating(AlarmManager.RTC_WAKEUP, getStartTime(context),
-                    AlarmManager.INTERVAL_DAY, mStartPendingIntent);
+                        AlarmManager.INTERVAL_DAY, mStartPendingIntent);
             }
         }
     }
@@ -250,5 +253,16 @@ public class Utils {
         assert am != null;
         am.cancel(endPendingIntent);
         am.cancel(startPendingIntent);
+    }
+
+    public static boolean threeButtonNavbarEnabled(Context context) {
+        boolean defaultToNavigationBar = context.getResources().getBoolean(
+                com.android.internal.R.bool.config_showNavigationBar);
+        boolean navigationBar = Settings.System.getInt(context.getContentResolver(),
+                Settings.System.FORCE_SHOW_NAVBAR, defaultToNavigationBar ? 1 : 0) == 1;
+        boolean hasNavbar = false;
+        hasNavbar = com.android.internal.util.du.Utils.isThemeEnabled(
+                "com.android.internal.systemui.navbar.threebutton") && navigationBar;
+        return hasNavbar;
     }
 }

pixeldust-project-caf/android_packages_apps_DU-Themes

Signed-off-by: spezi77 <spezi7713@gmx.net>

src/com/dirtyunicorns/themes/utils/Utils.java
@@ -42,10 +42,13 @@ import android.content.SharedPreferences;
 import android.content.om.IOverlayManager;
 import android.content.pm.PackageManager;
 import android.os.RemoteException;
+import android.provider.Settings;
 import android.text.TextUtils;
 import android.widget.Button;
 
 import androidx.preference.PreferenceManager;
+
+import com.android.internal.util.pixeldust.ThemesUtils;
 
 import com.dirtyunicorns.themes.R;
 import com.dirtyunicorns.themes.receivers.ThemesEndReceiver;
@@ -178,12 +181,12 @@ public class Utils {
                 .getBoolean(PREF_THEME_SCHEDULED_REPEAT_DAILY, false)) {
             if (mAlarmMgr != null) {
                 mAlarmMgr.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, getEndTime(context),
-                    mEndPendingIntent);
+                        mEndPendingIntent);
             }
         } else {
             if (mAlarmMgr != null) {
                 mAlarmMgr.setRepeating(AlarmManager.RTC_WAKEUP, getEndTime(context),
-                    AlarmManager.INTERVAL_DAY, mEndPendingIntent);
+                        AlarmManager.INTERVAL_DAY, mEndPendingIntent);
             }
         }
     }
@@ -196,12 +199,12 @@ public class Utils {
                 .getBoolean(PREF_THEME_SCHEDULED_REPEAT_DAILY, false)) {
             if (mAlarmMgr != null) {
                 mAlarmMgr.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, getStartTime(context),
-                    mStartPendingIntent);
+                        mStartPendingIntent);
             }
         } else {
             if (mAlarmMgr != null) {
                 mAlarmMgr.setRepeating(AlarmManager.RTC_WAKEUP, getStartTime(context),
-                    AlarmManager.INTERVAL_DAY, mStartPendingIntent);
+                        AlarmManager.INTERVAL_DAY, mStartPendingIntent);
             }
         }
     }
@@ -250,5 +253,16 @@ public class Utils {
         assert am != null;
         am.cancel(endPendingIntent);
         am.cancel(startPendingIntent);
+    }
+
+    public static boolean threeButtonNavbarEnabled(Context context) {
+        boolean defaultToNavigationBar = context.getResources().getBoolean(
+                com.android.internal.R.bool.config_showNavigationBar);
+        boolean navigationBar = Settings.System.getInt(context.getContentResolver(),
+                Settings.System.FORCE_SHOW_NAVBAR, defaultToNavigationBar ? 1 : 0) == 1;
+        boolean hasNavbar = false;
+        hasNavbar = com.android.internal.util.pixeldust.PixeldustUtils.isThemeEnabled(
+                "com.android.internal.systemui.navbar.threebutton") && navigationBar;
+        return hasNavbar;
     }
 }